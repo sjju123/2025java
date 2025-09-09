2eb6019c9ded38e0bd5eb4a5c33f2d886511f7dc
sjju123 <sjju0301@gmail.com>
2025-09-09 10:44:07 +0900

    0909 수업 실습

---
+++ CH02/src/sec01/exam04/ScopeTest.java
@@ -0,0 +1,20 @@
+package sec01.exam04;
+
+public class ScopeTest {
+
+	public static void main(String[] args) {
+		int a =10;
+		
+		
+		if(a>1) {
+			int b = a;
+		}
+		
+		for(int i =0; i<10; i++) {
+			int c = a + i;
+			c = b + i;
+		}
+		
+	}
+
+}
---
+++ CH02/src/sec03/exam01/PromotiomTest.java
@@ -0,0 +1,25 @@
+package sec03.exam01;
+
+public class PromotiomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte btyeVal = 10;
+		int intVal =  btyeVal;
+		
+		
+		char charVal ='A';
+		//char charVal = 65;
+		//System.out.print(charVal);
+		int intVall = charVal;
+		System.out.println(intVall);
+		
+		byte byteVal1 = 65;
+		int intVal2 =65;
+		char charVal1 = (char)byteVal1;
+		//char charVal2 = intVal2;
+		
+		
+	}
+
+}

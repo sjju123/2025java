19e5f82572c5caa70f826b807540fe7118d26d52
sjju123 <sjju0301@gmail.com>
2025-09-11 14:48:06 +0900

    2025java.09.11

---
+++ CH02/src/sec03/exam02/inTest.java
@@ -0,0 +1,36 @@
+package sec03.exam02;
+import java.util.Scanner;
+
+
+public class inTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte val1 = 1;
+		byte val2 = 2;
+		
+		//byte val3 = val1 + val2;
+		
+//		System.out.println("1/2:" + 1/2);
+//		System.out.println("1.0/2.0:" +1.0/2.0);
+		
+		String str ="10";
+		int value = 20;
+		//String str1 = str + value;
+		String str1 = "" + value;
+		
+		//int result = val1 + Integer.parseInt(str);
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		
+		
+		
+		System.out.println(input);
+		
+		
+
+	}
+
+}

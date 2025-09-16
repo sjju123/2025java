4df70cc487701d2bdc5f21434df2aa09ac05607b
sjju123 <sjju0301@gmail.com>
2025-09-16 10:39:52 +0900

    0916수업실습

---
+++ CH02/src/sec03/exam03/ValueTest.java
@@ -0,0 +1,19 @@
+package sec03.exam03;
+
+public class ValueTest {
+
+	public static void main(String[] args) {
+		int Value = 10;
+		String str = "3"+7+8;
+		//			"37+8"
+		str=7+8+"3";
+		//	15+"3"
+		str = ""+Value;
+		
+		
+		System.out.println(str);
+		
+		
+	}
+
+}
---
+++ Ch04/src/sec01/exam/SwichTest.java
@@ -0,0 +1,29 @@
+package sec01.exam;
+
+public class SwichTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String val = "Mon";
+		
+		switch(val) {
+		case "Mon":
+			System.out.println("Code 1");
+			break;
+		case "Tue":
+			System.out.println("Code 2");
+			break;
+		case "Wed":
+			System.out.println("Code 3");
+			break;
+		default:
+			System.out.println("No code");
+			break;
+			
+			
+		}
+
+	}
+
+}

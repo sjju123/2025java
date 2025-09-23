4ebf75cf62ef1be919b0fe8ae246485c67f1c1d5
sjju123 <sjju0301@gmail.com>
2025-09-23 10:48:24 +0900

    0923수업실습

---
+++ Ch05/src/sec01/exam01/Test.java
@@ -0,0 +1,21 @@
+package sec01.exam01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = a;
+		
+		String str1 = null;
+		String str2 = str1;
+		
+		System.out.println("Hi.");
+		
+//		int[] intArray = null;
+//		intArray[0] = 10;
+//		
+		
+
+	}
+
+}

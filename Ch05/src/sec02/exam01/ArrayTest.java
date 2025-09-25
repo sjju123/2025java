c38cfc37995a74024006c2573f5664b8fad02c7d
sjju123 <sjju0301@gmail.com>
2025-09-25 14:43:46 +0900

    0925수업실습

--- Ch05/src/sec02/exam01/ArrayTest.java
+++ Ch05/src/sec02/exam01/ArrayTest.java
@@ -11,8 +11,12 @@ public class ArrayTest {
 //			System.out.println(intArray[i]);
 //		}
 		
-		int[][] scores = new int [2][3];
-		scores [0] = new int[3];
+		//int[][] scores = new int [2][3]; 이 코드를 실행했을때 아래의 내용으로 결과물이 생긴다.
+		int[][] scores ;
+		scores = new int [2][];
+		scores [0] = new int[3]; // 스코어 1차원 배열중 0번쨰 배열에 새로운 정수를 3개 저장할수있는 공간을 만듬
+		scores [1] = new int[3]; // 위의 내용과 동일 하고 2번째 배열에 만듬
+		
 		
 		
 		
